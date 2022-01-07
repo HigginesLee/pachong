ce638acdc6ad311f4d21cf98f7d33e37ed330ee8
HigginsLee <kimitaer@gmail.com>
2022-01-07 20:28:53 +0800

    Add something

--- web_request.py
+++ web_request.py
@@ -6,3 +6,10 @@
 熟练使用浏览器抓包工具
 熟练使用抓包工具
 '''
+import requests
+url='https://www.sogou.com/web?query=周杰伦'
+headers={
+    'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36'
+}
+response=requests.get(url,headers=headers)
+print(response.text)
