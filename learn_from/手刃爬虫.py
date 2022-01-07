06896360d24d18ce73ee9305aae5dabbf2833a3d
HigginsLee <kimitaer@gmail.com>
2022-01-07 20:46:53 +0800

    Do something

---
+++ learn_from/手刃爬虫.py
@@ -0,0 +1,7 @@
+#user HigginesLee
+from urllib.request import urlopen
+url='http://148.100.78.8'
+req=urlopen(url)
+with open('./TestHtml.html','w') as f:
+    f.write(req.read().decode('utf-8'))
+print('Over')
